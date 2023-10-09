a8ad0f64cd0f2a96f1caec9da7fb374578b8f73b
adiutant <mikolaj@marasek.eu>
2023-10-09 16:12:08 +0200

    add an entry

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -4,7 +4,11 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-
+        <h1>31LO spotted</h1>
+        <form>
+            <label htmlFor={"entry"}></label>
+            <input type={"text"} id={"entry"} name={"entry"} placeholder={"napisz coś..."}></input>
+        </form>
     </div>
 
   )
